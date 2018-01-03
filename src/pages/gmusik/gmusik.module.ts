15bd2c256f2b559f1ac614ed8995603894bb8f66
Gabriel Mensah <reachgaby41@gmail.com>
2018-01-03 17:03:31 +0000

    i have inserted picture into underage page

---
+++ src/pages/gmusik/gmusik.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { GmusikPage } from './gmusik';
+
+@NgModule({
+  declarations: [
+    GmusikPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(GmusikPage),
+  ],
+})
+export class GmusikPageModule {}
